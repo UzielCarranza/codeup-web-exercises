dabdc5c4d6b68c948dbcf290f222a8195a0843d7
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-07 20:11:16 -0500

    created conditional_examples to moved old data from conditionals/. completed conditionals exercises from the codeup curriculum

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,206 +1,218 @@
 "use strict";
-//monday, february 7th, 2021
+
+/* ########################################################################## */
+
+/**
+ * TODO:
+ * Create a function named `analyzeColor` that accepts a string that is a color
+ * name as input. This function should return a message which relates to the
+ * color stated in the argument of the function. For colors you do not have
+ * responses written for, return a string stating so
+ *
+ * Example:
+ *  > analyzeColor('blue') // returns "blue is the color of the sky"
+ *  > analyzeColor('red') // returns "Strawberries are red"
+ *
+ *
+ *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+ *
+ * You should use an if-else-if-else block to return different messages.
+ *
+ * Test your function by passing various string literals to it and
+ * console.logging the function's return value
+ */
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color every time the page loads)
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 
-
-//example 1
-var isAdmin = false;
-
-if (isAdmin){
-    console.log("you are an admin");
+function analyzeColor(color){
+    if (color === "blue"){
+        return color + " as the sky";
+    } else if (color === "red"){
+        return color + " as an apple";
+    } else if (color === "orange"){
+        return color + " as an orange...idk";
+    } else if (color === "yellow"){
+        return color + " as a banana";
+    } else if (color === "green"){
+        return color + " as the grass";
+    } else if (color === "indigo"){
+        return color + " as something";
+    } else if (color === "violet"){
+        return color + " as something else...";
+    }  else {
+        return "i don't know this color...";
+    }
 }
-
-////example 2
-
-var itemPrice =  4.99;
-var moneyInWallet = 5.00;
-
-if(moneyInWallet >= itemPrice){
-    console.log("you can buy this items.log");
-} else {
-    console.log("you cannot buy this");
-}
-
-//example 3
-
-var age = 20;
-var priceOfDrink = 8.00;
-
-if (age >= 21 && moneyInWallet >= priceOfDrink){
-    console.log("you can buy the drink")
-} else {
-    console.log("you cannot buy a drink")
-}
-
-
-//mini-exercise
-/*
-IF / ELSE IF / ELSE
-Create a function called getToDestination which will help determine how a person can get to their destination!
-    It will accept 4 arguments and the parameter names are:
-    age - the age of the rider
-isInsured - a boolean telling the function if the rider has insurance
-hasCar - a boolean describing if the rider has a car
-canGetRideshare - boolean describing if the rider can get a ride share (Uber, etc)
-If the rider is over 16, has insurance, and has a car then they can use their own vehicle
-If the rider is not over 16, has no insurance, OR has no car but they CAN get a rideshare, then they call up a rideshare service
-If the rider meets none of the above, they should call a friend for a ride!
-    This getToDestination should not return any value, but rather console log based on the conditions met.
-    getToDestination, part 2
-Create a function called canGetRideshare() which will RETURN a boolean
-It will determine if there are drivers nearby and the person has enough money for the ride
-Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
-Call canGetRideshare() in the else if after !isLegalDriver &&...
-Be sure to log to the user if they can or cannot get a rideshare
-
+/**
+ * TODO:
+ * Pass the `randomColor` variable to your 'analyzeColor' function and console.log the results.
+ * You should see a different message every time you refresh the page
  */
 
-//walkthrough
+console.log(analyzeColor(randomColor))
 
-
-/*
-function getToDestination(age, isInsured, hasCar,){
-    if (age > 16 && isInsured && hasCar){
-        console.log("you can use your own vehicle")
-    } else if (canGetRideShare(true, true)){
-        //alternative
-        //else if (canGetRideShare)
-        console.log("you can get a rideshare")
-    } else {
-        console.log("better call a friend, sorry")
-    }
-}
+/**
+ * TODO:
+ * Comment out the code above, and refactor your function to use a switch-case statement
  */
 
-function getToDestination(age, isInsured, hasCar,hasMoney, driversNearby){
-    if (age > 16 && isInsured && hasCar){
-        console.log("you can use your own vehicle")
-    } else if (canGetRideShare(driversNearby,hasMoney)){
-        //alternative
-        //else if (canGetRideShare)
-        console.log("you can get a rideshare")
-    } else {
-        console.log("better call a friend, sorry")
-    }
-}
 
 
-
-
-getToDestination(17, false, true, true, true, )
-
-
-
-function canGetRideShare(hasDriversNearby, hasEnoughMoney){
-    if (hasDriversNearby && hasEnoughMoney){
-        return true;
-    } else {
-        return false;
-    }
-    //arternative
-    //retunr !!(hasDriversNearby, hasEnoughMoney)
-}
-
-//2nd exercise
-function checkWeather(todaysWeather){
-    if (todaysWeather === "snowing"){
-        console.log("please stay inside")
-    } else if (todaysWeather === "raining"){
-        console.log("grab an umbrella")
-    } else if (todaysWeather === "hailing"){
-        console.log("recover your car")
-    } else {
-        console.log("go play outside")
-    }
-}
-
-checkWeather("raining");
-
-//switch statement
-
-
-checkWeather("raining");
-
-function checkWeatherBreak(todaysWeather){
-    switch(todaysWeather){
-        case "snowing":
-            console.log("please stay inside");
+function analyzeColor(color){
+    switch (color) {
+        case "red":
+            return color + " as an apple";
             break;
-        case "raining":
-            console.log("grab umbrella");
+        case "orange":
+            return color + " as an orange...idk";
             break;
-        case "hailing":
-            console.log("cover your car");
+        case "yellow":
+            return color + " as a banana";
+            break;
+        case "green":
+            return color + " as the grass";
+            break;
+        case "blue":
+            return color + " as the sky";
+            break;
+        case "indigo":
+            return color + " as something";
             break;
         default:
-            console.log("go play outside");
+            return + "... i don't know this color...";
             break;
     }
 }
 
-
-checkWeatherBreak("raining");
-
+console.log(analyzeColor(randomColor));
 
 
 
+/**
+ * TODO:
+ * Prompt the user for a color when the page loads, and pass the input from the
+ * user to your `analyzeColor` function. Alert the return value from your
+ * function to show it to the user.
+ */
 
 
+let userAnswer = prompt("enter your favorite color...")
+console.log("answer is " + userAnswer);
+alert(analyzeColor(userAnswer));
 
+/* ########################################################################## */
 
+/**
+ * TODO:
+ * Suppose there's a promotion in Walmart, each customer is given a randomly
+ * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+ * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+ * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+ * everything for free!.
+ *
+ * Write a function named `calculateTotal` which accepts a lucky number and total
+ * amount, and returns the discounted price.
+ *
+ * Example:
+ * calculateTotal(0, 100) // returns 100
+ * calculateTotal(4, 100) // returns 50
+ * calculateTotal(5, 100) // returns 0
+ *
+ * Test your function by passing it various values and checking for the expected
+ * return value.
+ */
 
-
-
-/*
-//extra project to work on it... make it better
-
-//declare values
-let ageUser = 16
-let hasInsurance = false;
-let hasCar = true;
-
-
-//define whether or not to take a rideShare
-function getToDestination(riderAge, isInsured, hasCar){
-    if(riderAge >= 16 && (isInsured == false) && (hasCar == false || hasCar == true)
-        && canGetRideshare(driverAva== true && hasMoney == true)){
-        console.log("better to get a ride share!");
-    } else if (riderAge > 16 && ((isInsured == true) || (hasCar == true))){
-        console.log("you can use your own vehicle");
-    } else if (riderAge <= 15 && hasCar == true && hasInsurance == true){
-        console.log("better to call a friend");
-    }else {
-        console.log("just call a friend, sorry")
-    }
-}
-
-//call the function
-getToDestination(ageUser,hasInsurance,hasCar)
-console.log(canGetRideshare(driverAva, hasMoney));
-
-
-
-// //declare values for canGetRideshare()
-let driverAva = true;
-let hasMoney = true;
-
-
-//can get a rideShare based on funds and driver availability
-function canGetRideshare(driverAvailable,hasEnoughFunds){
-
-    if (driverAvailable && hasEnoughFunds){
-        return true;
+function calculateTotal(luckyNumber, totalAmount){
+    let discount;
+    if (luckyNumber == 0){
+        return "sorry, youu didn't get a discount";
+    } else if (luckyNumber == 1){
+        discount = (totalAmount * 10) / 100
+        return "your new total is: $" + (totalAmount - discount) + "." + " You received a dscount of 10%";
+    } else if (luckyNumber == 2){
+        discount = (totalAmount * 25) / 100
+        return "your new total is: $" + (totalAmount - discount) + "." + " You got a discount for 25%";
+    } else if (luckyNumber == 3){
+        discount = (totalAmount * 35) / 100
+        return "your new total is: $" + (totalAmount - discount) + "." + " You received a discount of 35%";
+    } else if (luckyNumber == 4){
+        discount = (totalAmount * 50) / 100
+        return "your new total is now " + (totalAmount - discount) + "." + " You received a discount of 50%";
+    } else if (luckyNumber == 5){
+        discount = (totalAmount * 100) / 100
+        return "congrats, you get 100% off: $" + (totalAmount - discount);
     } else {
         return false;
     }
 }
-
-canGetRideshare(driverAva,hasMoney)
-
+    console.log(calculateTotal(3, 100))
 
 
-console.log("you" + getToDestination(ageUser, hasInsurance, hasCar, canGetRideshare(driverAva, hasMoney)));
+
+
+
+/**
+ * TODO:
+ * Uncomment the line below to generate a random number between 0 and 5.
+ * (In this line of code, 0 is inclusive, and 6 is exclusive)
+ * Prompt the user for their total bill, then use your `calculateTotal` function
+ * and alerts to display to the user what their lucky number was, what their
+ * price before the discount was, and what their price after the discount is.
  */
 
+
+//Generate a random number between 0 and 6
+var luckyNumber = Math.floor(Math.random() * 6);
+let usertotalBill = parseInt(prompt("what is your total bill?"))
+alert("your lucky number was" + " " + luckyNumber)
+let userGetsDiscounted = calculateTotal(luckyNumber, usertotalBill);
+alert( userGetsDiscounted);
+
+
+/**
+ * TODO:
+ * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+ * would like to enter a number. If they click 'Ok', prompt the user for a
+ * number, then use 3 separate alerts to tell the user:
+ *
+ * - whether the number is even or odd
+ * - what the number plus 100 is
+ * - if the number is negative or positive
+ *
+ * Do *NOT* display any of the above information
+ * if the user enters a value that is not of the number data type.
+ * Instead, use an alert to inform them of the incorrect input data type.
+ *
+ *
+ * Can you refactor your code to use functions?
+ * HINT: The way we prompt for a value could be improved
+ */
+
+let userConfirms = confirm(" do you want to enter a number?")
+if (userConfirms){
+    let askUserForNumber = parseInt(prompt("enter your number"));
+    let sumOf100 = askUserForNumber + 100;
+    while (!askUserForNumber){
+        alert("please enter a valid number");
+        askUserForNumber = parseInt(prompt("enter your number"));
+        sumOf100 = askUserForNumber + 100;
+    }
+    if (askUserForNumber % 2 === 0){
+        alert(askUserForNumber + " is is even and adding 100 is " + sumOf100)
+    } else {
+        (askUserForNumber % 2 === 1)
+        alert("your number is odd and adding 100 is " + sumOf100)
+    } if (askUserForNumber > 0){
+        alert(askUserForNumber + " is positive")
+    } else {
+        alert(askUserForNumber + " is negative")
+    }
+}
 
 
---
+++ js/conditionals_examples.js
@@ -0,0 +1,206 @@
+"use strict";
+//monday, february 7th, 2021
+
+
+
+//example 1
+var isAdmin = false;
+
+if (isAdmin){
+    console.log("you are an admin");
+}
+
+////example 2
+
+var itemPrice =  4.99;
+var moneyInWallet = 5.00;
+
+if(moneyInWallet >= itemPrice){
+    console.log("you can buy this items.log");
+} else {
+    console.log("you cannot buy this");
+}
+
+//example 3
+
+var age = 20;
+var priceOfDrink = 8.00;
+
+if (age >= 21 && moneyInWallet >= priceOfDrink){
+    console.log("you can buy the drink")
+} else {
+    console.log("you cannot buy a drink")
+}
+
+
+//mini-exercise
+/*
+IF / ELSE IF / ELSE
+Create a function called getToDestination which will help determine how a person can get to their destination!
+    It will accept 4 arguments and the parameter names are:
+    age - the age of the rider
+isInsured - a boolean telling the function if the rider has insurance
+hasCar - a boolean describing if the rider has a car
+canGetRideshare - boolean describing if the rider can get a ride share (Uber, etc)
+If the rider is over 16, has insurance, and has a car then they can use their own vehicle
+If the rider is not over 16, has no insurance, OR has no car but they CAN get a rideshare, then they call up a rideshare service
+If the rider meets none of the above, they should call a friend for a ride!
+    This getToDestination should not return any value, but rather console log based on the conditions met.
+    getToDestination, part 2
+Create a function called canGetRideshare() which will RETURN a boolean
+It will determine if there are drivers nearby and the person has enough money for the ride
+Now, when you call getToDestination(), instead of passing in a canGetRideShare parameter
+Call canGetRideshare() in the else if after !isLegalDriver &&...
+Be sure to log to the user if they can or cannot get a rideshare
+
+ */
+
+//walkthrough
+
+
+/*
+function getToDestination(age, isInsured, hasCar,){
+    if (age > 16 && isInsured && hasCar){
+        console.log("you can use your own vehicle")
+    } else if (canGetRideShare(true, true)){
+        //alternative
+        //else if (canGetRideShare)
+        console.log("you can get a rideshare")
+    } else {
+        console.log("better call a friend, sorry")
+    }
+}
+ */
+
+function getToDestination(age, isInsured, hasCar,hasMoney, driversNearby){
+    if (age > 16 && isInsured && hasCar){
+        console.log("you can use your own vehicle")
+    } else if (canGetRideShare(driversNearby,hasMoney)){
+        //alternative
+        //else if (canGetRideShare)
+        console.log("you can get a rideshare")
+    } else {
+        console.log("better call a friend, sorry")
+    }
+}
+
+
+
+
+getToDestination(17, false, true, true, true, )
+
+
+
+function canGetRideShare(hasDriversNearby, hasEnoughMoney){
+    if (hasDriversNearby && hasEnoughMoney){
+        return true;
+    } else {
+        return false;
+    }
+    //arternative
+    //retunr !!(hasDriversNearby, hasEnoughMoney)
+}
+
+//2nd exercise
+function checkWeather(todaysWeather){
+    if (todaysWeather === "snowing"){
+        console.log("please stay inside")
+    } else if (todaysWeather === "raining"){
+        console.log("grab an umbrella")
+    } else if (todaysWeather === "hailing"){
+        console.log("recover your car")
+    } else {
+        console.log("go play outside")
+    }
+}
+
+checkWeather("raining");
+
+//switch statement
+
+
+checkWeather("raining");
+
+function checkWeatherBreak(todaysWeather){
+    switch(todaysWeather){
+        case "snowing":
+            console.log("please stay inside");
+            break;
+        case "raining":
+            console.log("grab umbrella");
+            break;
+        case "hailing":
+            console.log("cover your car");
+            break;
+        default:
+            console.log("go play outside");
+            break;
+    }
+}
+
+
+checkWeatherBreak("raining");
+
+
+
+
+
+
+
+
+
+
+
+/*
+//extra project to work on it... make it better
+
+//declare values
+let ageUser = 16
+let hasInsurance = false;
+let hasCar = true;
+
+
+//define whether or not to take a rideShare
+function getToDestination(riderAge, isInsured, hasCar){
+    if(riderAge >= 16 && (isInsured == false) && (hasCar == false || hasCar == true)
+        && canGetRideshare(driverAva== true && hasMoney == true)){
+        console.log("better to get a ride share!");
+    } else if (riderAge > 16 && ((isInsured == true) || (hasCar == true))){
+        console.log("you can use your own vehicle");
+    } else if (riderAge <= 15 && hasCar == true && hasInsurance == true){
+        console.log("better to call a friend");
+    }else {
+        console.log("just call a friend, sorry")
+    }
+}
+
+//call the function
+getToDestination(ageUser,hasInsurance,hasCar)
+console.log(canGetRideshare(driverAva, hasMoney));
+
+
+
+// //declare values for canGetRideshare()
+let driverAva = true;
+let hasMoney = true;
+
+
+//can get a rideShare based on funds and driver availability
+function canGetRideshare(driverAvailable,hasEnoughFunds){
+
+    if (driverAvailable && hasEnoughFunds){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+canGetRideshare(driverAva,hasMoney)
+
+
+
+console.log("you" + getToDestination(ageUser, hasInsurance, hasCar, canGetRideshare(driverAva, hasMoney)));
+ */
+
+
+
