a17dc8ce9a950e0f6b3afff6e688f254e397f3e8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-09 20:40:42 -0500

    completed extra exercises for while loops

--- js/whileLoops_extraExercises.js
+++ js/whileLoops_extraExercises.js
@@ -60,13 +60,20 @@ getFizzBuzz(30);
 //     HINT: start by defining the correct password and don't forget to link your html file
 
 let secretPassword = "password"
-
-
-function getPassword(){
-
+let isCorrect = false;
+let attemp = "";
+function getPassword(pass){
+    while(!isCorrect){
+        attemp = prompt("enter your password");
+        isCorrect = attemp === secretPassword;
+        if (!isCorrect){
+            alert("not the right password")
+        } else {
+            alert("password correct!!")
+        }
+    }
 }
-
-
+getPassword();
 
 
 
@@ -75,10 +82,30 @@ function getPassword(){
 //     This function will use a while loop to compare its current number (a variable named counter) against the meaningfulNum.
 //     While the counter is not equivalent to the meaningfulNum, continue the loop.
 //     Once that condition is broken, console log a string telling the user "The meaning of life, the universe, and everything is [your meaningful number]"
+
+function getMeaningOfLife(meaningfulNum){
+    let counter = 41;
+    while(counter !== meaningfulNum || meaningfulNum === 'string'){
+        console.log("will never know...");
+        break;
+    } if (counter === meaningfulNum){
+        console.log("The meaning of life, the universe, and everything is " + meaningfulNum)
+    } else if (meaningfulNum === 42){
+        console.log("Did you bring your towel?")
+    }
+}
+
+getMeaningOfLife(42);
+
+
 // Thinking about the above function, getMeaningOfLife, let's ask a question: What happens if the value of meaningfulNum is a non-numeric?
 // Write some code to account for this case. How the function handles the situation is up to you!
+
+
+//created a break and log will never know
+
+
+
+
 //     Let's continue with getMeaningOfLife: If the value of meaningfulNum happens to be 42, console log a separate message at the end of the function to say: "Did you bring your towel?"
 // LESSON EXERCISE WALK-THRU
-// --> While Loops
-//
-// --> Break and Continue?
