d6ec4714cf983f608516ae5ed9bc37c582c1bbbf
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-10 17:01:25 -0500

    completed exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,22 @@
+
+
+
+function oddNumber() {
+    let counting = 50;
+    do {
+        userInput = parseInt(prompt("please enter an odd number!"));
+        if (userInput % 2 === 1) {
+            break;
+        }
+    } while (userInput % 2 === 0 || isNaN(userInput))
+
+    for (let i = 1; i <= counting;i+= 2) {
+        if (userInput === i){
+            console.log("we don't want this number!!! " + userInput)
+            continue;
+        }
+        console.log(i)
+    }
+}
+
+console.log(oddNumber())
