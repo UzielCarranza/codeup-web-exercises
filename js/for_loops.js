bcec1cd2a1fe656029493a63c77481001cf77589
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-10 10:40:02 -0500

    worked on exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,15 +1,21 @@
 "use strict";
 
 function showMultiplicationTable(number){
-let multiply;
-    for (var i = 1; i < number; i++) {
-        multiply = number[i] * i;
-        console.log( + i + ', x = ');
+ let multiply;
+ let counter;
+//     for (var i = 1; i < number; i++) {
+//         multiply = number[i] * i;
+//         console.log( + i + ', x = ');
+//     }
+    for (var i = 1, j = 1; i < 10, j < 10; i++, j++) {
+        counter = number[i];
+        multiply = ( i * j);
+        console.log(i + " * " + j + ' = ' + multiply);
     }
+
 }
 
-showMultiplicationTable(9);
-
+console.log(showMultiplicationTable(5))
 //
 // for (var i = 0, j = 9; i < 10; i++, j--) {
 //     console.log('for loop iteration #' + i + ', j = ' + j);
