61c1cf49abfa3fb9d2aceeb66b86d0f056765313
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-10 13:01:27 -0500

    completed second exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,11 +1,25 @@
 "use strict";
+//
+// function showMultiplicationTable(number){
+//     let multiply;
+//         for (var j = 1; j <= 10; j++) {
+//             multiply = (number * j);
+//             console.log(number + " * " + j + ' = ' + multiply);
+//         }
+// }
+//
+// console.log(showMultiplicationTable(5))
 
-function showMultiplicationTable(number){
-    let multiply;
-        for (var j = 1; j <= 10; j++) {
-            multiply = (number * j);
-            console.log(number + " * " + j + ' = ' + multiply);
-        }
+
+
+//second exercise
+let randomNums = Math.floor(Math.random() * 200) + 20;
+console.log(randomNums);
+
+for (var i = 0; i < randomNums; i++){
+    if (i % 2 === 0){
+        console.log(i + " is even")
+    } else {
+        console.log(i + " is odd")
+    }
 }
-
-console.log(showMultiplicationTable(5))
