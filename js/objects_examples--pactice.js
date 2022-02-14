52820bdaa72cb784aaa47b6a83b6331f67c4d451
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-14 12:59:12 -0500

    created obj

--- js/objects_examples--pactice.js
+++ js/objects_examples--pactice.js
@@ -1,11 +1,25 @@
+
+//crate obj with properties
 let car = {};
 console.log(typeof car);
 
-
+//assign properties
 car.make = 'ford';
 console.log(car);
 
+//assign properties
 car.model = 'mustang';
 console.log(car);
 
 console.log(car.model + " this is the car model");
+
+
+
+//create obj with propertie
+let cohort = {
+    name: 'fortran',
+    startDate: '02/27/2022',
+    endDate: '07/31/2022'
+}
+
+console.log(cohort.name);
