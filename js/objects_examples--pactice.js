28888a2e01070974bf32d6333824d158a1e93169
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-14 12:53:15 -0500

    created practice files

---
+++ js/objects_examples--pactice.js
@@ -0,0 +1,11 @@
+let car = {};
+console.log(typeof car);
+
+
+car.make = 'ford';
+console.log(car);
+
+car.model = 'mustang';
+console.log(car);
+
+console.log(car.model + " this is the car model");
