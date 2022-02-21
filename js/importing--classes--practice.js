02bc64562a5203c231a61c44abb3ba5ef9609cf0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-21 12:05:11 -0500

    completed exporting, importing classes to create new object

--- js/importing--classes--practice.js
+++ js/importing--classes--practice.js
@@ -2,7 +2,16 @@
  * Create a class for the Backpack object type.
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
+import Computer from "../js/objectClass--practice.js"
 
-import Computer from "./objectClass--practice";
-
-const workLaptop = new Computer()
+const workLaptop = new Computer(
+    "uzielcaa09",
+    "medium",
+    'macOS',
+    'silver',
+    'uzielca',
+    'uziel2',
+    true,
+    '25%'
+);
+console.log("uziel's work omputer", workLaptop);
