faf100b838ec94ad07e3ec5ef96111531e4d6d73
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-21 11:29:02 -0500

    added parameters

---
+++ js/objectClass--practice.js
@@ -0,0 +1,15 @@
+class computer{
+    constructor(
+        //define parameters:
+        user,
+        size,
+        os,
+        color,
+        user1Name,
+        user2Name,
+        isOn,
+        batteryPercentage
+    ) {
+        //define properties:
+    }
+}
