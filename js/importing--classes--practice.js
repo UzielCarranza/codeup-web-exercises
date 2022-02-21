df8a78a561538f0b62e165066e1181d06dda8d08
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-21 11:40:05 -0500

    importing classes to js file

---
+++ js/importing--classes--practice.js
@@ -0,0 +1,7 @@
+/**
+ * Create a class for the Backpack object type.
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ */
+
+import computer from "./objectClass--practice";
+
