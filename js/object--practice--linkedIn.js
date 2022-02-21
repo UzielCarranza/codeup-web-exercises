e88701c85c7f3fc3643a515d3adc16bf450453ed
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-21 10:06:48 -0500

    completed first exercise

--- js/object--practice--linkedIn.js
+++ js/object--practice--linkedIn.js
@@ -7,3 +7,20 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const backPack = {
+    size: "medium",
+    numOfPockets: 4,
+    strapLength: {
+        strapLeft: 4,
+        strapRight: 4
+    },
+    color: "red",
+    isOpen: false,
+    3: "invalid input",
+}
+
+console.log(backPack);
+console.log("the backpack is color:", backPack.color);
+console.log("the length of the left strap is:",backPack.strapLength.strapLeft)
+console.log(backPack["3"]);
