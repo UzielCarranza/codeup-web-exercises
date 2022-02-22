02a3df97f83c0d2b55edb04df9908c1b5eaae8cb
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-02-21 19:04:51 -0500

    added a nav through the use of DOM

--- js/importing--classes--practice.js
+++ js/importing--classes--practice.js
@@ -18,6 +18,7 @@ const workLaptop = new Computer(
 console.log("uziel's work omputer", workLaptop);
 console.log('days since acquired', workLaptop.computerAge());
 
+
 const content = `
 <main>
     <article>
@@ -35,4 +36,60 @@ const content = `
 </main>
 `;
 
-document.body.innerHTML = content;
+// document.body.innerHTML = content;
+
+//old way
+//string output
+// const content2 = "<h1>" + workLaptop.user + "</h1>";
+
+
+//add classes and attributes to an existint div
+const main = document.querySelector(".maincontent");
+
+const newDiv = document.createElement("div");
+newDiv.classList.add("someClass");
+newDiv.setAttribute('id', 'just another class');
+newDiv.innerHTML = content;
+
+main.append(newDiv);
+
+/*
+* add a navigation to the DOM
+* */
+
+const navContent = `
+<li><a href="#">home</a></li>
+
+<li><a href="#">About</a></li>
+
+<li><a href="#">Backpackcs</a></li>
+
+<li><a href="#">Other things</a></li>
+
+<li><a href="#">Contact</a></li>
+`;
+
+const mainNav = document.createElement("nav");
+mainNav.classList.add("main-navigation");
+const navList = document.createElement("ul");
+navList.innerHTML = navContent;
+mainNav.append(navList);
+
+document.querySelector(".class").append(mainNav);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
