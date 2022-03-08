008ea4a3280cb75d43d87556a2bf07028f999d09
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-07 22:03:03 -0500

    completed exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+var contents = $('#codeup').html();
+alert(contents);
+
+console.log(contents);
+
+$('.important').css('background-color', 'yellow');
+
+$('p').css('font-size', '40px');
