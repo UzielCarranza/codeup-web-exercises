41c74e32e7107dc9b9387134887e5d399cc13ae1
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 16:37:20 -0500

    added hover functionality to lis

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -105,15 +105,28 @@
 
 //mouse events
 
+//Add jQuery code that will change the background color of an h1 element when clicked.
 $('h1').click(function() {
  $(this).css('background-color', "#7FFFD4")
 });
 
+//Make all paragraphs have a font size of 18px when they are double clicked.
 $('p').dblclick(function(e) {
  $(this).css('font-size', "18px")
 });
 
 
+//Set all li text color to red when the mouse is hovering; reset to black when it is not.
+let liBgC = $('li').css("color")
+console.log(liBgC)
+$('li').hover(
+    function() {
+     $(this).css('color', '#DC143C');
+    },
+    function() {
+     $(this).css('color', liBgC);
+    }
+);
 
 
 
