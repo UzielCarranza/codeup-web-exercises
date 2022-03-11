3eca3ba78351b7d0adfeb70780b666e4f06a93c1
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 11:42:49 -0500

    completed exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -8,6 +8,22 @@ alert(contents);
 console.log(contents);
 //
 
+//Update the jQuery code to select and alert a different id.
+let contents2 = $('#ul').html()
+alert(contents2, "content of a different ID");
+console.log(contents2);
+//
+
+
+// Use the same id on 2 elements. How does this change the jQuery selection?
+// let contents3 = $('#ul').html()
+// alert(contents3, "content of a different ID");
+// console.log(contents3);
+
+//I added an ID to a different element, used the code above to alert the content but instead I got
+//the same content as of the first element with the ID.
+
+
 
 $('.important').css('background-color', 'yellow');
 
