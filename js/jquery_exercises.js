c147bd1e93327230ebf9e74d63a23b7c2cf1c543
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 15:22:30 -0500

    completed exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,17 +1,31 @@
-$(document).ready(function() {
+// $(document).ready(function() {
     // alert( 'The DOM has finished loading!' );
-});
+// });
+
+
+// $('body').append(getTemplate("username"))
+//
+// function getTemplate(){
+//     return `
+//     <div>
+//     <h1>template</h1>
+//     <p>template ${some} literals</p>
+//
+//     </div
+//     `
+// }
+
 
 //Use jQuery to select an element by the id. Alert the contents of the element.
-let contents = $('#codeup').html();
+// let contents = $('#codeup').html();
 // alert(contents);
-console.log(contents);
+// console.log(contents);
 //
 
 //Update the jQuery code to select and alert a different id.
-let contents2 = $('#ul').html()
-// alert(contents2, "content of a different ID");
-console.log(contents2);
+// let contents2 = $('#ul').html()
+// alert(contents2);
+// console.log(contents2);
 //
 
 
@@ -37,36 +51,39 @@ console.log(contents2);
 //
 // $("div").addClass('codeup')
 
-$('h1, p, div').attr("class","codeup");
+// $('h1, p, div').attr("class","codeup");
 
 
 
 // Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
 
-$('.codeup').css('border', '2px solid red');
+// $('.codeup').css('border', '2px solid red');
 
 // Remove the class from one of the elements. Refresh and test that the border has been removed.
 // $("h1").removeClass(["codeup"]);
 
-$("h1").removeClass('codeup');
 
 // Give another element an id of codeup. Does this element get a border now?
-$('li').attr("id","newId");
+// $('li').attr("id","newId");
 
 //it didnt get the styling
 
 
 
 
-//element selector
+//element selectors
 // Remove your custom jQuery code from previous exercises.
 // Using jQuery, set the font-size of all li elements to 20px.
 
-$('li').css("font-size", "20px")
+$('li').css("font-size", "20px");
 
 
 // Craft selectors that highlight all the h1, p, and li elements.
+$("h1").css('background-color', 'yellow');
 
+$("p").css('background-color', 'yellow');
+
+$("li").css('background-color', 'yellow');
 
 // Create a jQuery statement to alert the contents of your h1 element(s).
 let contentsh1 = $('h1').html()
@@ -81,4 +98,4 @@ console.log(contentsh1);
 //multiple selector
 // Combine your selectors that highlight all the h1, p, and li elements.
 
-$('h1, p, li').css('background-color', 'yellow');
+// $('h1, p, li').css('background-color', 'yellow');
