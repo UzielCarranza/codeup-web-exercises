558b236c3e3be80f4febaea861a17c72ab657391
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 11:31:00 -0500

    alerted the content of an ID selector

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
 
-var contents = $('#codeup').html();
+//Use jQuery to select an element by the id. Alert the contents of the element.
+let contents = $('#codeup').html();
 alert(contents);
-
 console.log(contents);
+//
+
 
 $('.important').css('background-color', 'yellow');
 
