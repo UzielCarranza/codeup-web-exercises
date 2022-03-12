2306a21e0c0e65abfe284b4dbf8c5e5a3f310855
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 19:00:15 -0500

    moved function to external js

--- js/konami.js
+++ js/konami.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+
+    let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+    let userPosition = 0;
+    $(document).keyup(function (event) {
+        console.log(event.keyCode);
+        let key = konamiCode[userPosition];
+        let keyPressed = event.keyCode;
+        if (key === keyPressed){
+            userPosition++;
+            if (userPosition === konamiCode.length){
+                $('body').css("background-color", "black")
+            }
+        }
+    });
+})
+
+
 
 const addSection = () => {
     return `
