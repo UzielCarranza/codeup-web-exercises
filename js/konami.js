1e712ab2cbcb37268d123dfe37e27e31f98698af
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 20:27:17 -0500

    toggle function triggers when konamiCode === true

--- js/konami.js
+++ js/konami.js
@@ -1,9 +1,20 @@
+
+const addSection = () => {
+    return `
+    <details>
+    <summary>Here are some cheat cheat codes</summary>
+    <p><i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-left-long"></i>   <i class="fa-solid fa-arrow-right-long"></i> <i class="fa-solid fa-left-long"></i>  <i class="fa-solid fa-arrow-right-long"></i> 
+    <br> <span>b</span> a enter </p>
+    </details>
+    `;
+};
+
 const imageAdd = () => {
     return `
     <h1 width="50%" style="margin-top:3em; margin-left: 25% ">Enjoy!!!</h1>
 <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
     <iframe src="https://giphy.com/embed/LjULRGiyt1KpO" width="100%" height="50%" style="position:absolute; margin-top:1em; margin-left: 0%"
-            frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+            frameBorder="0"></iframe>
 </div>
 <span>
 
@@ -15,35 +26,7 @@ Your browser does not support the audio element.
     `;
 };
 
-$(document).ready(function () {
 
-    let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-    let userPosition = 0;
-    $(document).keyup(function (event) {
-        console.log(event.keyCode);
-        let key = konamiCode[userPosition];
-        let keyPressed = event.keyCode;
-        if (key === keyPressed){
-            userPosition++;
-            if (userPosition === konamiCode.length){
-                $('body').css('background-color', "#000")
-                $('#video').append(imageAdd)
-            }
-        }
-    });
-})
-
-
-
-const addSection = () => {
-    return `
-    <details>
-    <summary>Here are some cheat cheat codes</summary>
-    <p><i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-left-long"></i>   <i class="fa-solid fa-arrow-right-long"></i> <i class="fa-solid fa-left-long"></i>  <i class="fa-solid fa-arrow-right-long"></i> 
-    <br> <span>b</span> a enter </p>
-    </details>
-    `;
-};
 
 $('#add').click(function() {
 
@@ -60,7 +43,7 @@ $('#add').click(function() {
 
 let contrastToggle = false;
 
-function toggleContrast(){
+let toggleContrast = () =>{
     contrastToggle = !contrastToggle;
     if (contrastToggle){
         document.body.classList += " dark-theme"
@@ -69,5 +52,26 @@ function toggleContrast(){
         document.body.classList.remove("dark-theme")
     }
 }
+
+
+$(document).ready(function () {
+
+    let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+    let userPosition = 0;
+    $(document).keyup(function (event) {
+        console.log(event.keyCode);
+        let key = konamiCode[userPosition];
+        let keyPressed = event.keyCode;
+        if (key === keyPressed){
+            userPosition++;
+            if (userPosition === konamiCode.length){
+                toggleContrast()
+                $('details').remove();
+                $('#video').append(imageAdd)
+            }
+        }
+    });
+})
+
 
 
