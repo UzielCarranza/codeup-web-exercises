bfb73ac3fba57a0aebea912f71109a328300b6bf
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-12 12:05:17 -0500

    practice jquery

---
+++ js/jquery-practice.js
@@ -0,0 +1,20 @@
+// $("col-4").hover(function (){
+//     $(this).attr('card')
+// })
+//
+// let text = $('.col-4').html()
+// console.log(text);
+//
+// $('.col-4').hover(function (){
+//     $(this).attr(text)
+    //
+    // $('.col-4').html(text)
+// })
+
+// $('.col-4').html(text)
+
+
+$('main div:first-child').hover(function (){
+   let content =  $(this).html();
+    console.log(content)
+});
