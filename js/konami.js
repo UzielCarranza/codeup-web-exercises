6b86dc119bf8503016099fcd8dde26cf07169911
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-11 19:41:29 -0500

    added video background

--- js/konami.js
+++ js/konami.js
@@ -1,3 +1,12 @@
+const imageAdd = () => {
+    return `
+<div style="width:100%;height:0;padding-bottom:75%;position:relative;">
+    <iframe src="https://giphy.com/embed/LjULRGiyt1KpO" width="50%" height="50%" style="position:absolute; margin-top:4em; margin-left: 25%"
+            frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+</div>
+    `;
+};
+
 $(document).ready(function () {
 
     let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
@@ -9,7 +18,7 @@ $(document).ready(function () {
         if (key === keyPressed){
             userPosition++;
             if (userPosition === konamiCode.length){
-                $('body').css("background-color", "black")
+                $('#video').append(imageAdd)
             }
         }
     });
@@ -19,12 +28,23 @@ $(document).ready(function () {
 
 const addSection = () => {
     return `
-    <summary>The code</summary>
+    <details>
+    <summary>Here are some cheat cheat codes</summary>
+    <p><i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-up"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-arrow-down-long"></i>  <i class="fa-solid fa-left-long"></i>   <i class="fa-solid fa-arrow-right-long"></i> <i class="fa-solid fa-left-long"></i>  <i class="fa-solid fa-arrow-right-long"></i> 
+    <br> <span>b</span> a enter </p>
+    </details>
     `;
-}
+};
 
 $('#add').click(function() {
 
-    $('#section').append(addSection)
+    $('body').append(addSection);
+    $('details').css('text-align', "center")
+    $('summary').css('font-size', "2em")
+    $('.fa-solid').css('font-size', "2em")
+    $('p').css('font-size', "4em")
 });
+
+
+
 
