34ec88d6da2a6df7482157553dc313ad6d98fd9c
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-29 13:11:22 -0400

    get userName and search on github completed

--- js/promises.js
+++ js/promises.js
@@ -1,18 +1,24 @@
 
 
 const url = 'https://api.github.com/repos/UzielCarranza/codeup-web-exercises/commits';
-// https://api.github.com/repos/gmPuryear/codeup-web-exercises/commits
-
-fetch(url, {headers: {'Authorization': GIT_HUB}})
-    .then(data => data.json())
-    .then(data => console.log(data[0].commit.author.date))
 
 
+getLastCommit(getUserName())
+getCommetLastCommit(getUserName())
+function getUserName(){
+    return "UzielCarranza"
+}
+function getLastCommit(username) {
+    fetch(`https://api.github.com/repos/${username}/codeup-web-exercises/commits`,
+        {headers: {'Authorization': GIT_HUB}})
+        .then(data => data.json())
+        .then(data => console.log(data[0].commit.author.date))
+}
 
 
-const url2 = 'https://api.github.com/users/UzielCarranza/events';
-// https://api.github.com/repos/gmPuryear/codeup-web-exercises/commits
 
-fetch(url2, {headers: {'Authorization': GIT_HUB}})
-    .then(data => data.json())
-    .then(data => console.log(data[0].payload.commits[0].message))
+function getCommetLastCommit(username) {
+    fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': GIT_HUB}})
+        .then(data => data.json())
+        .then(data => console.log(data[0].payload.commits[0].message))
+}
